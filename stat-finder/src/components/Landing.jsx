c2448fb652a6cd123e684ed24c9e9c1a816f6f56
anthony-gudiel <adg15@sfu.ca>
2025-07-11 11:31:29 -0700

    Beginning of landing page

---
+++ stat-finder/src/components/Landing.jsx
@@ -0,0 +1,19 @@
+import "../styles/landing.css"
+
+export default function Landing() {
+    return (
+        <main>
+            <div className="tagline">
+                <h1>Do yourself a favour by making more informed bets.</h1>
+                <h3>Most bets are made on gut feeling. We help you back it up with real trends by
+                highlighting how players have performed against the line in recent games.</h3>
+            </div>
+
+            <div className="landing-container">
+                <p>Hello</p>
+                <p>Hello</p>
+            </div>
+        </main>
+    )
+}
+
