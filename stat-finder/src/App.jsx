ab172ec5f39174eaee435b22d7096f147a6ae674
anthony-gudiel <adg15@sfu.ca>
2025-07-15 14:51:44 -0700

    Added bare bones, hard coded player card

--- stat-finder/src/App.jsx
+++ stat-finder/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import Header from "./components/Header"
 import Landing from "./components/LandingMain"
+import PlayerStats from './components/PlayerStats'
 
 function App() {
 
   return (
     <>
       <Header />
-      <Landing />
+      {/* <Landing /> */}
+      <PlayerStats />
     </>
   )
 }
